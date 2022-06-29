3d40ce3685730bc26c5ab134e6125b714dc7b544
Islam El-Ashi <islam.elashi@dfinity.org>
2022-06-29 16:05:44 +0200

    Add new endpoints

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # Bitcoin Integration Demo
 
-A demo of the `bitcoin_get_balance` and `bitcoin_get_utxos` endpoints.
+A demo of the bitcoin endpoints on the Internet Computer.
 
 This demo is already deployed to the IC, so you can already try it yourself. Examples:
+
+## `get_balance`
 
 ```
 dfx canister --network=ic call kmpi4-4aaaa-aaaal-aaqba-cai get_balance '("tb1qsgx55dp6gn53tsmyjjv4c2ye403hgxynxs0dnm")'
 ```
 
+## `get_utxos`
+
 ```
-dfx canister --network=ic call kmpi4-4aaaa-aaaal-aaqba-cai get_utxos '("tb1qc7psdze9j0r38rv8gj2kl8gysqevtqyqs20upw")'
+dfx canister --network=ic call kmpi4-4aaaa-aaaal-aaqba-cai get_utxos '("tb1qc7psdze9j0r38rv8gj2kl8gysqevtqyqs20upw", null)'
 ```
 
-Note that, at the time of this writing, when calling `get_utxos` with thousands of UTXOs, one may
-get a "Payload too large" error. This is an error that will be rectified in a future replica
-upgrade.
+In cases where an address has a large number of UTXOs, these may require pagination. If a `next_page` is provided in the response, then it can be retrieved as follows:
 
+```
+dfx canister --network=ic call kmpi4-4aaaa-aaaal-aaqba-cai get_utxos '("tb1qsgx55dp6gn53tsmyjjv4c2ye403hgxynxs0dnm", opt blob "<response.next_page>"
+```
+
+## `get_current_fee_percentiles`
+
+```
+dfx canister --network=ic call kmpi4-4aaaa-aaaal-aaqba-cai get_current_fee_percentiles
+```
+
+## `send_transaction`
+
+```
+dfx canister --network=ic call kmpi4-4aaaa-aaaal-aaqba-cai send_transaction '(blob "<raw transaction>")'
+```
--- src/bitcoin_integration_demo/src/lib.rs
+++ src/bitcoin_integration_demo/src/lib.rs
@@ -1,10 +1,23 @@
-use ic_btc_types::{GetBalanceRequest, GetUtxosRequest, GetUtxosResponse, Network};
-use ic_cdk::export::Principal;
+use ic_btc_types::{
+    GetBalanceRequest, GetCurrentFeePercentilesRequest, GetUtxosRequest, GetUtxosResponse,
+    MillisatoshiPerByte, Network, Page, SendTransactionRequest, UtxosFilter,
+};
+use ic_cdk::{api::call::call_with_payment, export::Principal};
 use ic_cdk_macros::update;
+
+const M: u64 = 1_000_000; // One million
+const B: u64 = 1_000_000_000; // One billion
+
+// Fees for the various bitcoin endpoints.
+const GET_BALANCE_FEE: u64 = 100 * M;
+const GET_UTXOS_FEE: u64 = 100 * M;
+const GET_CURRENT_FEE_PERCENTILES_FEE: u64 = 100 * M;
+const SEND_TRANSACTION_BASE_FEE: u64 = 5 * B;
+const SEND_TRANSACTION_FEE_PER_BYTE: u64 = 20 * M;
 
 #[update]
 async fn get_balance(address: String) -> u64 {
-    let balance_res: Result<(u64,), _> = ic_cdk::api::call::call(
+    let balance_res: Result<(u64,), _> = call_with_payment(
         Principal::management_canister(),
         "bitcoin_get_balance",
         (GetBalanceRequest {
@@ -12,6 +25,7 @@ async fn get_balance(address: String) -> u64 {
             network: Network::Testnet,
             min_confirmations: None,
         },),
+        GET_BALANCE_FEE,
     )
     .await;
 
@@ -19,17 +33,57 @@ async fn get_balance(address: String) -> u64 {
 }
 
 #[update]
-async fn get_utxos(address: String) -> GetUtxosResponse {
-    let utxos_res: Result<(GetUtxosResponse,), _> = ic_cdk::api::call::call(
+async fn get_utxos(address: String, page: Option<Page>) -> GetUtxosResponse {
+    let utxos_res: Result<(GetUtxosResponse,), _> = call_with_payment(
         Principal::management_canister(),
         "bitcoin_get_utxos",
         (GetUtxosRequest {
             address,
             network: Network::Testnet,
-            filter: None,
+            filter: page.map(UtxosFilter::Page),
         },),
+        GET_UTXOS_FEE,
     )
     .await;
 
     utxos_res.unwrap().0
+}
+
+#[update]
+async fn get_current_fee_percentiles() -> Vec<MillisatoshiPerByte> {
+    let res: Result<(Vec<MillisatoshiPerByte>,), _> = call_with_payment(
+        Principal::management_canister(),
+        "bitcoin_get_current_fee_percentiles",
+        (GetCurrentFeePercentilesRequest {
+            network: Network::Testnet,
+        },),
+        GET_CURRENT_FEE_PERCENTILES_FEE,
+    )
+    .await;
+
+    res.unwrap().0
+}
+
+#[update]
+async fn send_transaction(transaction: Vec<u8>) {
+    // A crude check so that we don't spend too many cycles.
+    if transaction.len() > 1000 {
+        panic!("Transaction too large.");
+    }
+
+    let fee =
+        SEND_TRANSACTION_BASE_FEE + (transaction.len() as u64) * SEND_TRANSACTION_FEE_PER_BYTE;
+
+    let res: Result<(), _> = call_with_payment(
+        Principal::management_canister(),
+        "bitcoin_send_transaction",
+        (SendTransactionRequest {
+            network: Network::Testnet,
+            transaction,
+        },),
+        fee,
+    )
+    .await;
+
+    res.unwrap()
 }
